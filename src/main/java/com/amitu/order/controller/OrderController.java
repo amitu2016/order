5afa076f13bdd0ac46fb5dfefe6999b31eddba7e
amit-macbook <amiu2016@gmail.com>
2025-02-15 22:07:30 +0530

    allow cross origin

--- src/main/java/com/amitu/order/controller/OrderController.java
+++ src/main/java/com/amitu/order/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.amitu.order.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import com.amitu.order.service.OrderService;
 
 @RestController
 @RequestMapping("/order")
+@CrossOrigin
 public class OrderController {
 	
 	private final OrderService orderService;
